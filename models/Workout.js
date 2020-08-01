ce806d13791701be56e32039940cefa116108fb6
Myhyun <mattyihyun@gmail.com>
2020-08-01 12:59:20 -0700

    inital workout model working

---
+++ models/Workout.js
@@ -0,0 +1,20 @@
+module.exports = {};
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+    },
+    exercises: {
+        type: [
+
+        ]
+    },
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
