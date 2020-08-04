8e0c69f5c1728eeb201fbf574f571110746cfd4e
Myhyun <mattyihyun@gmail.com>
2020-08-03 18:51:10 -0700

    added post and put api routes for workouts

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,15 +5,35 @@ router.get("/api/workouts", (req, res) => {
     db.Workout.find()
         .sort({ day: 1 })
         .then((dbWorkout) => {
-            res.json(dbWorkout.map((w) => w.toObject({virtuals: true})));
+            res.json(dbWorkout.map((w) => w.toObject({ virtuals: true })));
         })
         .catch(err => {
             res.status(400).json(err);
         });
 });
 
-router.get("/api/exercises", (req, res) => {
+router.post("/api/workouts", (req, res) => {
+    db.Workout.create()
+        .then((data) => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
-})
+router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+        req.params.id,
+        { $push: { exercises: req.body } },
+        { new: true, runValidators: true }
+    )
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
 
 module.exports = router;
